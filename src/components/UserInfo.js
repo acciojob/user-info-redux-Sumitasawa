85486107475a0bf224c83f87c97e9a184ce1bf08
Sumitasawa <sumitasawa65@gmail.com>
2025-11-12 18:30:14 +0530

    First

---
+++ src/components/UserInfo.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { updateEmail,updateName } from '../userSlice'
+import { useDispatch,useSelector } from 'react-redux'
+const UserInfo = () => {
+    const dispatch=useDispatch();
+    const {name,email}=useSelector((state)=>state.user);
+
+
+    const handlenameChange=(e)=>{
+        dispatch(updateName(e.target.value));
+    }
+
+    const handleemailChange=(e)=>{
+        dispatch(updateEmail(e.target.value));
+    }
+  return (
+    <>
+    <div>
+      <h1>User Information</h1>
+      <label htmlFor='name'>Name:</label>
+      <input
+        id="name"
+        type="text"
+        value={name}
+        onChange={handlenameChange}
+       />
+       </div>
+
+        <div>
+       <label htmlFor='email'>Email:</label>
+       <input 
+        id="email"
+        type="email"
+        value={email}
+        onChange={handleemailChange}
+       />
+    </div>
+
+
+    <div>
+        <p>Current values in store:</p>
+        <p>Name-{name}</p>
+        <p>Email-{email}</p>
+    </div>
+    </>
+
+
+  )
+}
+
+export default UserInfo
